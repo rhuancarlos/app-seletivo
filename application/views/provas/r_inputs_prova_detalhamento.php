8b8cc50f754d1a350bef1ae63edceffb5c68da09
Rhuan Carlos <rhuancarlosg12@gmail.com>
2020-11-20 18:07:31 -0300

    001

--- application/views/provas/r_inputs_prova_detalhamento.php
+++
@@ -1,9 +0,0 @@
-<div class="row">
-	<div class="col-md-12">
-		<div class="form-group">
-			<label for="">Mais detalhes sobre a prova</label>
-			<textarea name="prova_detalhamento" id="prova_detalhamento" style="width: 100%; height:100px;"></textarea>
-			<div class="help-block form-text with-errors form-control-feedback"></div>
-		</div>
-	</div>	
-</div>
